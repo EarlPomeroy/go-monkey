88f259cebc3b339d2d9da4127480602a5ffea06b
Earl Pomeroy <earl.pomeroy42@gmail.com>
2021-12-20 14:33:40 -0700

    Added Evaluation
    Eval of infix, prefix,
    program and statuements , if/else, returns, errors, boolean and integer, and Nils

---
+++ object/object.go
@@ -0,0 +1,56 @@
+package object
+
+import "fmt"
+
+type ObjectType string
+
+const (
+	INTEGER_OBJ      = "INTEGER"
+	BOOLEAN_OBJ      = "BOOLEAN"
+	NIL_OBJ          = "NIL"
+	RETURN_VALUE_OBJ = "RETURN_VAL"
+	ERROR_OBJ        = "ERROR"
+)
+
+type Object interface {
+	Type() ObjectType
+	Inspect() string
+}
+
+type Integer struct {
+	Value int64
+}
+
+func (i *Integer) Inspect() string { return fmt.Sprintf("%d", i.Value) }
+
+func (i *Integer) Type() ObjectType { return INTEGER_OBJ }
+
+type Boolean struct {
+	Value bool
+}
+
+func (b *Boolean) Inspect() string { return fmt.Sprintf("%t", b.Value) }
+
+func (b *Boolean) Type() ObjectType { return BOOLEAN_OBJ }
+
+type Nil struct{}
+
+func (n *Nil) Inspect() string { return "nil" }
+
+func (n *Nil) Type() ObjectType { return NIL_OBJ }
+
+type ReturnValue struct {
+	Value Object
+}
+
+func (rv *ReturnValue) Type() ObjectType { return RETURN_VALUE_OBJ }
+
+func (rv *ReturnValue) Inspect() string { return rv.Value.Inspect() }
+
+type Error struct {
+	Message string
+}
+
+func (e *Error) Type() ObjectType { return ERROR_OBJ }
+
+func (e *Error) Inspect() string { return "Error: " + e.Message }
